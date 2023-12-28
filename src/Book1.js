ef8416b42fb39a1e43b54f3a3dcac42ff0b1347f
Ahad <Mdahadkhan178@gmail.com>
2023-12-28 23:13:59 +0530

    fix_image

--- src/Book1.js
+++ src/Book1.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./index.css";
 
 const Book1 = (props) => {
   return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Book1 from "./Book1";
-import "./App.css";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
